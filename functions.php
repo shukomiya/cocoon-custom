0268f2a872002efad6e604fff08812ec3b722990
shu <shu@komish.jp>
2019-07-09 17:19:59 +0900

    fix functions.php

--- functions.php
+++ functions.php
@@ -5,3 +5,28 @@ if ( !defined( 'ABSPATH' ) ) exit;
 add_editor_style();
 
 //以下に子テーマ用の関数を書く
+$g_domain_name = $_SERVER["SERVER_NAME"];
+$g_is_localhost = $g_domain_name == 'localhost';
+
+if ( $g_is_localhost ) {
+	$g_analy_g_acount = 'UA-4079996-8';
+	$g_domain_name = 'komish.com';
+	$g_category_nav = false;
+	$g_ad_enabled = false;
+	$g_is_localhost = false;
+} else {
+	if ( $g_domain_name === 'komish.com' ) {
+		$g_analy_g_acount = 'UA-4079996-8';
+		$g_category_nav = false;
+		$g_ad_enabled = true;
+	} else if ( $g_domain_name === 'plus.komish.com' ) {
+		$g_analy_g_acount = 'UA-4079996-23';
+		$g_category_nav = true;
+		$g_ad_enabled = false;
+	}
+}
+
+// for debug
+//$g_ad_enabled = true;
+//$g_category_nav = true;
+
