ee9fbf4c01ee89d319bb3ba4dbe9263d5d999314
shu <shu@komish.jp>
2019-07-09 17:09:39 +0900

    fix

--- tmp/header-sales-letter-full.php
+++ tmp/header-sales-letter-full.php
@@ -19,16 +19,6 @@ if ( get_google_search_console_id() ): ?>
 <meta name="google-site-verification" content="<?php echo get_google_search_console_id() ?>" />
 <!-- /Google Search Console -->
 <?php endif;//Google Search Console終了 ?>
-<?php //Google Tag Manager
-if (is_analytics() && $tracking_id = get_google_tag_manager_tracking_id()): ?>
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','<?php echo $tracking_id; ?>');</script>
-<!-- End Google Tag Manager -->
-<?php endif //Google Tag Manager終了 ?>
 
 <?php // force Internet Explorer to use the latest rendering engine available ?>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
